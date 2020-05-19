e2e65eccac066bff4a51a3635a40ae04be9483c7
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-05-19 19:10:25 -0400

    completed doubly_linked_list.py

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -93,12 +93,18 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return
+        self.add_to_head(node.value)
+        self.delete(node)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return
+        self.add_to_tail(node.value)
+        self.delete(node)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
@@ -123,4 +129,14 @@ class DoublyLinkedList:
         
     """Returns the highest value currently in the list"""
     def get_max(self):
-        pass
+        # create max variable
+        current = self.head
+        max = self.head.value
+        # Loop through nodes
+        while(current is not None):
+            # compare value in node to max found
+            if current.value > max:
+                max = current.value
+            current = current.next
+        # return max found
+        return max
